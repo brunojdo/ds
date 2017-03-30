8a23b4a27007c614128687675014b6044a90cd5d
Bruno Dias <contato@diasbruno.com>
2017-03-30 16:01:41 -0300

    Add Decorator

---
+++ decorator/tabajara/Imagem.java
@@ -0,0 +1,18 @@
+package software_design.decorator.tabajara;
+
+import java.io.IOException;
+
+/**
+ * Exemplo da motivação para uso de wrappers.
+ */
+interface Imagem {
+    /**
+     * Desenha a imagem.
+     */
+    void draw() throws IOException;
+    
+    /**
+     * @return o conteúdo do imagem, sem cabeçalho.
+     */
+    byte[][] content() throws IOException;
+}
